392936f03b120cf2bd5666961f5e35bf1dcea908
Anton Blanchard <anton@ozlabs.org>
2020-12-13 21:00:11 +1100

    Add SYNTH_READ_BLACKBOX_LIB to microwatt configuration

--- openlane/microwatt/config.tcl
+++ openlane/microwatt/config.tcl
@@ -7,6 +7,7 @@ set ::env(FP_PDN_CORE_RING) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
 set ::env(VERILOG_FILES) "$script_dir/../../verilog/rtl/microwatt.v"
+set ::env(SYNTH_READ_BLACKBOX_LIB) 1
 
 set ::env(CLOCK_PORT) "ext_clk"
 set ::env(CLOCK_PERIOD) "20.000"
